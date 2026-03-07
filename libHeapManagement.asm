0ffc2593e6cc11cdce1bacf476414184c801a649
NOIRET Maxime <maxnrt@outlook.fr>
2026-03-07 22:20:57 +0100

    dev: Finished implementing malloc

--- libHeapManagement.asm
+++ libHeapManagement.asm
@@ -66,7 +66,7 @@ jr    $ra                    # return ($v0 is already what we want from sbrk sys
 #     $v0: Pointer to allocated space.
 # Registers used:
 #     $t0: $a1 even check -> free list iterator -> header pointer
-#     $t1: current chunk size -> less-than bool -> original chunk size -> new unallocated split size
+#     $t1: current chunk size -> less-than bool -> original chunk size -> new unallocated split size -> prev ptr -> next ptr
 #     $t2: footer pointer -> split header pointer
 #     $t3: split footer pointer
 # Note:
@@ -92,6 +92,7 @@ heap_malloc_not_found:
 break 1                                      # if no chunks work, FOR NOW, break execution
 
 heap_malloc_found:
+# update found chunk
 or    $v0, $zero, $t0
 addiu $v0, $v0, 4                            # set return value to header+4 (chunk data pointer)
 lw    $t1, 0($t0)                            # save original chunk size
@@ -103,6 +104,7 @@ addu  $t2, $t2, $a1                          # (header + 4) + new_size = footer
 ori   $a1, $a1, 1                            # mark chunk as allocated
 sw    $a1, 0($t2)                            # store new size in footer
 addi  $a1, $a1, -1                           # remove allocated bit from size for calculation
+# create split chunk
 subu  $t1, $t1, $a1                          # original_chunk_size - new_size
 addiu $t1, $t1, -8                           # (original_chunk_size - new_size) - 8 = new unallocated split size
 addiu $t2, $t2, 4                            # get split header pointer
@@ -110,12 +112,23 @@ sw    $t1, 0($t2)                            # store split size in split header
 addiu $t3, $t2, 4                            # skip header
 addu  $t3, $t3, $t1                          # go to split footer
 sw    $t1, 0($t3)                            # store split size in split footer
+# move prev and next into new split chunk and update free list
+lw    $t1, 4($t0)                            # get prev pointer
+sw    $t1, 4($t2)                            # store it in new split chunk
+beq   $t1, $zero, heap_malloc_prevptr_null   # if null, update first_free
+sw    $t2, 8($t1)                            # else, update prev's next to new split chunk's header pointer
+heap_malloc_update_next:
+lw    $t1, 8($t0)                            # get next pointer
+sw    $t1, 8($t2)                            # store it in new split chunk
+beq   $t1, $zero, heap_malloc_return         # if null, return
+sw    $t2, 4($t1)                            # else, update next's prev to new split chunk's header pointer
+j     heap_malloc_return
 
-# TODO:
-#   - If prev is null, update first_free
-#   - Else, update prev's next pointer
-#   - If next not null, update next's prev pointer
+heap_malloc_prevptr_null:                    # if prev ptr was null,
+sw    $t2, 4($a0)                            #   update first_free with split chunk header pointer
+j     heap_malloc_update_next
 
+heap_malloc_return:
 jr   $ra                                     # return
   
 

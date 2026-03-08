c8881ca8acb3a333f675795102bc65723844ec1d
NOIRET Maxime <maxnrt@outlook.fr>
2026-03-08 05:43:31 +0100

    dev: Added free() macros

--- macrosHeapManagement.asm
+++ macrosHeapManagement.asm
@@ -2,8 +2,8 @@
   jal heap_init
 .end_macro
 
-.macro malloc_RI(%reg, %size)
-  or  $a0, $zero, %reg
+.macro malloc_RI(%heap_reg, %size)
+  or  $a0, $zero, %heap_reg
   ori $a1, $zero, %size
   jal heap_malloc
  .end_macro
@@ -11,4 +11,15 @@
   lw  $a0, %heap_label
   ori $a1, $zero, %size
   jal heap_malloc
+.end_macro
+
+.macro free_RR(%heap_reg, %p)
+  or  $a0, $zero, %heap_reg
+  or  $a1, $zero, %p
+  jal heap_free
+.end_macro
+.macro free_LR(%heap_label, %p)
+  lw  $a0, %heap_label
+  or  $a1, $zero, %p
+  jal heap_free
 .end_macro

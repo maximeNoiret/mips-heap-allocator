e7de5768d5fe73403811fe6142e229ab03ff0122
NOIRET Maxime <maxnrt@outlook.fr>
2026-03-08 08:09:00 +0100

    test: Added free() prevFree+bothFree case test

--- testHeapManagement.asm
+++ testHeapManagement.asm
@@ -7,25 +7,29 @@ heap_start: .word 0
 
 
 main:
-addi $sp, $sp, -8            # allocate enough in the stack to fit every pointers
+addi $sp, $sp, -12            # allocate enough in the stack to fit every pointers
 heap_init()
 sw   $v0, heap_start         # save heap_start pointer
 
 malloc_LI(heap_start, 24)
-sw   $v0, 4($sp)             # store pointer to allocated space in stack
+sw   $v0, 8($sp)             # store pointer to allocated space in stack
 
 malloc_LI(heap_start, 32)    # allocate a second chunk to test first+alloc
-sw   $v0, 0($sp)
+sw   $v0, 4($sp)
 malloc_LI(heap_start, 16)
+sw   $v0, 0($sp)
 
 # test writing to allocated space
-lw   $t0, 4($sp)             # get space pointer
+lw   $t0, 8($sp)             # get space pointer
 ori  $t1, $t1, 100           # value 100
-sw   $t1, 4($t0)             # store 100 in allocated space
+sw   $t1, 8($t0)             # store 100 in allocated space
 
 free_LR(heap_start, $t0)     # test free firstChunk
-lw   $t0, 0($sp)
+malloc_LI(heap_start, 8)     # malloc to test prevFree+bothFree
+lw   $t0, 4($sp)
 free_LR(heap_start, $t0)     # test free prevFree
+lw   $t0, 0($sp)
+free_LR(heap_start, $t0)     # test free prevFree+bothFree
 
 ori	 $v0, $zero, 10          # load syscall code 10 (exit)
 syscall                      # exit

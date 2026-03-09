1e4e581cf306fdacdc85a12b690432b97b5d3f8a
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2026-03-09 01:57:24 +0100

    test: tested both nextFree cases

--- testHeapManagement.asm
+++ testHeapManagement.asm
@@ -6,30 +6,29 @@ heap_start: .word 0
 .include "macrosHeapManagement.asm"
 
 
+
+# Offsets
+# - 8: first allocated chunk
+# - 4: second allocated chunk
+# - 0: third allocated chunk
+
 main:
 addi $sp, $sp, -12            # allocate enough in the stack to fit every pointers
 heap_init()
 sw   $v0, heap_start         # save heap_start pointer
 
-malloc_LI(heap_start, 24)
-sw   $v0, 8($sp)             # store pointer to allocated space in stack
-
-malloc_LI(heap_start, 32)    # allocate a second chunk to test first+alloc
+malloc_LI(heap_start, 128)
+sw   $v0, 8($sp)
+malloc_LI(heap_start, 32)
 sw   $v0, 4($sp)
-malloc_LI(heap_start, 16)
+
+lw   $t0, 8($sp)
+free_LR(heap_start, $t0)     # free first chunk to leave space
+malloc_LI(heap_start, 8)     # allocate less space to test nextFree
+sw   $v0, 8($sp)
+malloc_LI(heap_start, 16)    # allocate less space to free
 sw   $v0, 0($sp)
-
-# test writing to allocated space
-lw   $t0, 8($sp)             # get space pointer
-ori  $t1, $t1, 100           # value 100
-sw   $t1, 8($t0)             # store 100 in allocated space
-
-free_LR(heap_start, $t0)     # test free firstChunk
-malloc_LI(heap_start, 8)     # malloc to test prevFree+bothFree
-lw   $t0, 4($sp)
-free_LR(heap_start, $t0)     # test free prevFree
-lw   $t0, 0($sp)
-free_LR(heap_start, $t0)     # test free prevFree+bothFree
+free_LR(heap_start, $v0)     # test free: [ALLOC],[TARGET],[UNALLOC],[ALLOC],[UNALLOC]->
 
 ori	 $v0, $zero, 10          # load syscall code 10 (exit)
 syscall                      # exit

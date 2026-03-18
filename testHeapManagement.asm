76cc4e8f9b8b2b4c5a6908237cad94fb65808e77
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2026-03-18 12:27:31 +0100

    test: switched test to use macros

--- testHeapManagement.asm
+++ testHeapManagement.asm
@@ -1,11 +1,11 @@
+.include "macrosHeapManagement.asm"
+
 main:
-jal  heap_init               # call heap_init
+heap_init()
 or   $s0, $zero, $v0         # save heap_start pointer
 
-or   $a0, $zero, $s0         # set heap_start as arg0
-ori  $a1, $zero, 24          # set 24 as size to allocate
-jal  heap_malloc             # malloc($s0, 24)
-addi $sp, $sp, -4            # allocate 4 bytes in stack
+malloc_RI($s0, 24)
+addi $sp, $sp, -4            # allocate a word in stack
 sw   $v0, 0($sp)             # store pointer to allocated space in stack
 
 # test writing to allocated space

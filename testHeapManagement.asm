b24f9df7016b94f95ba2b1ad3ac09ac2d91737f2
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2026-03-18 12:27:32 +0100

    doc: changed single char

--- testHeapManagement.asm
+++ testHeapManagement.asm
@@ -3,8 +3,8 @@ heap_start: .word 0
 
 
 .text
-
 .include "macrosHeapManagement.asm"
+
 
 main:
 heap_init()
@@ -17,12 +17,10 @@ sw   $v0, 0($sp)             # store pointer to allocated space in stack
 # test writing to allocated space
 lw   $t0, 0($sp)             # get space pointer
 ori  $t1, $t1, 100           # value 100
-sw   $t1, 0($t0)             # store 100 in allocated space?
-
+sw   $t1, 0($t0)             # store 100 in allocated space
 
 ori	 $v0, $zero, 10          # load syscall code 10 (exit)
 syscall                      # exit
-
 
 
 .include "libHeapManagement.asm"

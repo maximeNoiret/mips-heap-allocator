ee40b49d78738df2425227bd351c4807fe2e53f8
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2026-03-18 12:27:30 +0100

    dev: Added macro malloc REGISTER IMMEDIATE

--- macrosHeapManagement.asm
+++ macrosHeapManagement.asm
@@ -2,7 +2,11 @@
   jal heap_init
 .end_macro
 
-
+.macro malloc_RI(%reg, %size)
+  or  $a0, $zero, %reg
+  ori $a1, $zero, %size
+  jal heap_malloc
+ .end_macro
 .macro malloc_LI(%heap_label, %size)
   la  $a0, %heap_label
   ori $a1, $zero, %size

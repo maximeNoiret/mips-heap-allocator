43071f1128b5c3f9af7bc5faa0088391d477cd25
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2026-03-20 10:46:33 +0100

    dev: Wrote test for chunk realloc expension

--- testHeapManagement.asm
+++ testHeapManagement.asm
@@ -22,46 +22,14 @@ sw   $v0, heap
 
 addi $sp, $sp, -64
 
-print("test malloc first chunk\n")
-malloc_LI(heap, 128)
+print("Malloc 16 bytes\n")
+malloc_LI(heap, 16)
 sw    $v0, 60($sp)
-print("test malloc normal\n")
-malloc_LI(heap, 32)
-sw    $v0, 56($sp)
-malloc_LI(heap, 32)
-sw    $v0, 52($sp)
-malloc_LI(heap, 32)
-sw    $v0, 48($sp)
-malloc_LI(heap, 32)
-sw    $v0, 44($sp)
-malloc_LI(heap, 32)
-sw    $v0, 40($sp)
-malloc_LI(heap, 32)
-sw    $v0, 36($sp)
-malloc_LI(heap, 32)
-sw    $v0, 32($sp)
-malloc_LI(heap, 32)
-sw    $v0, 28($sp)
 
-print("test free first chunk\n")
-lw    $t0, 60($sp)
-free_LR(heap, $t0)
+print("test Realloc to 32 bytes\n")
+lw    $v0, 60($sp)
+realloc_LRI(heap, $v0, 32)
 
-print("test free prev_free\n")
-lw    $t0, 56($sp)
-free_LR(heap, $t0)
-
-print("test free no_free\n")
-lw    $t0, 44($sp)
-free_LR(heap, $t0)
-
-print("test free next_free\n")
-lw    $t0, 48($sp)
-free_LR(heap, $t0)
-
-print("test free next_last\n")
-lw    $t0, 28($sp)
-free_LR(heap, $t0)
 
 # exit
 ori   $v0, $zero, 10

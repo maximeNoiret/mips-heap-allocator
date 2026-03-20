54e3f13b11472dd37f438284ed0a4c98be9292d9
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2026-03-20 11:11:59 +0100

    test: Added test for chunk shrinking via realloc

--- testHeapManagement.asm
+++ testHeapManagement.asm
@@ -1,5 +1,6 @@
 .data
 heap: .word 0
+cleanup: .asciiz "\n\nCleanup\n\n"
 
 
 .text
@@ -13,14 +14,20 @@ heap: .word 0
   ori $v0, $zero, 4
   la $a0, text
   syscall
-.end_macro 
+.end_macro
+
+.macro printL(%label)
+  ori   $v0, $zero, 4
+  la    $a0, cleanup
+  syscall
+.end_macro
 
 main:
 
 heap_init()
-sw   $v0, heap
+sw    $v0, heap
 
-addi $sp, $sp, -64
+addiu $sp, $sp, -64
 
 print("Malloc 16 bytes\n")
 malloc_LI(heap, 16)
@@ -30,8 +37,21 @@ print("test Realloc to 32 bytes\n")
 lw    $v0, 60($sp)
 realloc_LRI(heap, $v0, 32)
 
+printL(cleanup)
+lw    $v0, 60($sp)
+free_LR(heap, $v0)
+
+print("Malloc 128 bytes\n")
+malloc_LI(heap, 128)
+sw    $v0, 60($sp)
+
+print("test Realloc to 64 bytes\n")
+lw    $v0, 60($sp)
+realloc_LRI(heap, $v0, 64)
+
 
 # exit
+addiu $sp, $sp, 64
 ori   $v0, $zero, 10
 syscall
 
